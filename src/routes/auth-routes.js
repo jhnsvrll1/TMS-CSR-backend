c44a892483482e2663d8f58eabce1f5cfa2dfd54
jhnsvrll1 <johannesvittorio@charis.sch.id>
2026-02-20 11:56:44 +0700

    new route for refresh token

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -7,5 +7,6 @@ const authController  = require('../controllers/authController');
 router.post('/login', authController.login);
 router.post('/register', authController.registerAdmin);
 router.post('/logout', authController.logout);
+router.post('/refresh-token', authController.refreshToken);
 
 module.exports = router;
